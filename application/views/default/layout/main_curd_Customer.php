416bd87d8a6f8ed65c72e947cdc18b86fcf46fc9
handesk <handeskim@users.noreply.github.com>
2018-01-14 13:20:03 +0700

    add layout

---
+++ application/views/default/layout/main_curd_Customer.php
@@ -0,0 +1,17 @@
+<section class="content">
+<div class="row">
+  <div class="col-md-2 col-sm-6 col-xs-12"><a target="_blank" href="<?php echo base_url()."excel_export?code=".$excel_command; ?>" ><span class="info-box-icon bg-yellow"><i class="fa fa-file-excel-o"></i></span></div></a>
+  <div class="col-md-4 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-user-secret"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Total customers</span>
+              <span class="info-box-number">{total_customer}</span>
+            </div>
+          </div>
+        </div>
+  <div class="col-md-12 col-sm-12 col-xs-12">
+      <?php echo $content; ?>
+  </div>
+</div>
+</section>
---
+++ application/views/default/print.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="http://medicine.io/public/bower_components/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://medicine.io/public/bower_components/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="http://medicine.io/public/bower_components/Ionicons/css/ionicons.min.css">
+
+
+
+<?php 
+    var_dump($response);
+    // foreach ($response as $key => $value){
+    //     echo $key. '===' .$value[$key];
+    // }
+?>
